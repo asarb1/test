82fa22d06aa10f42d3b0c80d4af0e67dca55e29d
Andreea's MacBook <andreeasmacbook@Andreeass-MacBook.local>
2023-12-07 12:06:04 +0200

    routes

--- app/routes/edit.routes.js
+++ app/routes/edit.routes.js
@@ -2,9 +2,9 @@ module.exports = app => {
   const imagine = require("../controllers/imagine.controller.js");
    var router = require("express").Router();
 
-  router.post("/", imagine.create);
-  router.delete("/", imagine.delete);
-  router.get("/", imagine.findOne);
-  router.get("/", imagine.findAllPublished);
+  router.post("/:id", imagine.create);
+  router.delete("/:id", imagine.delete);
+  router.get("/:id", imagine.findOne);
+  router.get("/:id", imagine.findAllPublished);
   app.use('/api/edit', router);
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   const imagine = require("../controllers/imagine.controller.js");
    var router = require("express").Router();
-  router.get("/", imagine.findAllPublished);
+  router.get("/:id", imagine.findAllPublished);
   app.use('/api/user', router);
 };
